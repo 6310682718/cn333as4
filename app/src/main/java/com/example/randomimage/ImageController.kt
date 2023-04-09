e77317fdfbfd6fe0b0a06ae9312b09795e097740
thanapat <thanapatspongpipat@gmail.com>
2023-04-09 17:17:59 +0700

    filter by category and get image by generate button

--- app/src/main/java/com/example/randomimage/ImageController.kt
+++ app/src/main/java/com/example/randomimage/ImageController.kt
@@ -1,44 +1,106 @@
 package com.example.randomimage
 
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
-import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.toSize
 import coil.compose.AsyncImage
+import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
+import coil.size.Size
 
+@Composable
+fun generateImage(width: Int, height: Int, imageCategory: String) {
+    Column(
+        // in this column we are adding modifier
+        // to fill max size, mz height and max width
+        modifier = Modifier
+            .fillMaxSize()
+            .fillMaxHeight()
+            .fillMaxWidth()
+            // on below line we are adding
+            // padding from all sides.
+            .padding(10.dp),
+        // on below line we are adding vertical
+        // and horizontal arrangement.
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        // on below line we are adding image for our image view.
+        Image(
+            // on below line we are adding the image url
+            // from which we will  be loading our image.
+            painter = rememberAsyncImagePainter("https://loremflickr.com/${width}/${height}/${imageCategory}"),
 
-fun generateImage(width: Int, height: Int, imageCategory: String): String {
-    if(width < 8 || height < 8){
-        return "Width & Height must greater than 8 !!";
+            // on below line we are adding content
+            // description for our image.
+            contentDescription = "gfg image",
+
+            // on below line we are adding modifier for our
+            // image as wrap content for height and width.
+            modifier = Modifier
+                .wrapContentSize()
+                .wrapContentHeight()
+                .wrapContentWidth()
+        )
     }
-    return "";
 }
 
 
 @Composable
 fun FormImageScreen(modifier: Modifier = Modifier) {
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+
+    val list = listOf("dog", "cat", "boy", "girl")
+    var selectedItem by remember {
+        mutableStateOf("")
+    }
+
+
+    var textFiledSize by remember {
+        mutableStateOf(Size)
+    }
+
+    val icon = if (expanded){
+        Icons.Filled.KeyboardArrowUp
+    }else{
+        Icons.Filled.KeyboardArrowDown
+    }
+
+    var imageUri by remember { mutableStateOf<Uri?>(null) }
+
+    val launcher =
+        rememberLauncherForActivityResult(contract = ActivityResultContracts.GetContent()) { uri: Uri? ->
+            imageUri = uri
+        }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -47,7 +109,9 @@ fun FormImageScreen(modifier: Modifier = Modifier) {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
             var width by remember { mutableStateOf(8) };
+            var widthres by remember { mutableStateOf(8) };
             var height by remember { mutableStateOf(8) };
+            var heightres by remember { mutableStateOf(8) };
             var warning by remember { mutableStateOf("") };
         Spacer(Modifier.height(30.dp))
             Text(text = "Random Image", style = TextStyle(
@@ -55,12 +119,40 @@ fun FormImageScreen(modifier: Modifier = Modifier) {
                 fontWeight = FontWeight.Bold,
                 color = Color.DarkGray,
             ))
+
+        Column(Modifier.padding(top = 20.dp)) {
+            OutlinedTextField(
+                value = selectedItem,
+                onValueChange = { selectedItem = it },
+                modifier = Modifier
+                    .fillMaxWidth(),
+                label = { Text(text = "Select Category") },
+                trailingIcon = {
+                    Icon(icon, "", Modifier.clickable { expanded = !expanded})
+                }
+            )
+
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false },
+            ) {
+                list.forEach { label ->
+                    DropdownMenuItem(onClick = {
+                        selectedItem = label
+                        expanded = false
+                    }) {
+                        Text(text = (label))
+                    }
+                }
+            }
+        }
+
         Spacer(Modifier.height(50.dp))
             TextField(
-                value = width.toString(),
+                value = widthres.toString(),
                 onValueChange = {
                     var res = it.toIntOrNull() ?: 0;
-                    width = res;
+                    widthres = res;
                 },
                 label = {
                     Text("Width : ")
@@ -74,10 +166,10 @@ fun FormImageScreen(modifier: Modifier = Modifier) {
             );
         Spacer(Modifier.height(20.dp))
             TextField(
-                value = height.toString(),
+                value = heightres.toString(),
                 onValueChange = {
                     var res = it.toIntOrNull() ?: 0;
-                    height = res;
+                    heightres = res;
                 },
                 label = {
                     Text("Height : ")
@@ -98,7 +190,7 @@ fun FormImageScreen(modifier: Modifier = Modifier) {
         Spacer(Modifier.height(20.dp))
         AsyncImage(
             model = ImageRequest.Builder(LocalContext.current)
-                .data("https://api.lorem.space/image/movie?w=${width}&h=${height}")
+                .data("https://loremflickr.com/${width}/${height}/${selectedItem}")
                 .crossfade(true)
                 .build(),
 //            model = "https://api.lorem.space/image/movie?w=150&h=220",
@@ -112,8 +204,10 @@ fun FormImageScreen(modifier: Modifier = Modifier) {
         Spacer(Modifier.height(20.dp))
         Button(
             onClick = {
-                warning = generateImage(width, height, "image");
-//                    numGuess = "";
+                height = heightres
+                width = widthres
+
+                //val painter = rememberAsyncImagePainter("https://loremflickr.com/${width}/${height}/${selectedItem}")
             }) {
             Text(text = "Generate", fontSize = 18.sp)
         }
